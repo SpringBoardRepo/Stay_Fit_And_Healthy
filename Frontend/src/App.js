fa116bf832cc4616b1622ec5e38a68cd3a8fa3eb
poorvarathore05 <poorvarathore05@gmail.com>
2021-12-29 18:58:35 -0800

    function for Login and signup form

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,24 +1,78 @@
-import Home from './Home';
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import RoutesComponents from './RoutesComponents';
+import MyfitnessApi from "./api";
 import NavBar from './NavBar';
-import Meals from './Meals';
-import BMICalculator from './BMICalculator';
-import Login from './Login'
-import Signup from './Signup';
+import { useEffect, useState } from "react";
+import useLocalStorage from "./useLocalStorage";
+import jwt_decode from "jwt-decode"
+import UserContext from "./UserContext";
+
+// Key name for storing token in localStorage for "remember me" re-login
+export const TOKEN_STORAGE_ID = "myfitness-token";
 
 function App() {
+
+  const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(function loadUserInfo() {
+
+    async function getCurrentUser() {
+      if (token) {
+        try {
+          console.log(JSON.stringify(token));
+          let user = jwt_decode(token, { complete: true });
+          console.log("USER", user);
+          MyfitnessApi.token = token;
+          let currentUser = await MyfitnessApi.getCurrentUser(user.username);
+          console.log(JSON.stringify("CURRENTUSER", currentUser));
+          setCurrentUser(currentUser);
+        } catch (error) {
+          console.error(error);
+          setCurrentUser(null);
+        }
+      }
+    }
+    getCurrentUser();
+  }, [token]);
+
+  async function login(logindata) {
+    try {
+      console.log(`LOGINDATA ${logindata}`);
+      let token = await MyfitnessApi.login(logindata);
+      console.log(`TOKEN ${token}`);
+      setToken(token);
+    } catch (error) {
+      console.error("login failed", error);
+      return { success: false, error };
+    }
+  }
+
+  async function signUp(signdata) {
+    try {
+      let token = await MyfitnessApi.signup(signdata);
+      setToken(token);
+    } catch (error) {
+      console.error("signUp failed", error);
+      return { success: false, error };
+    }
+  }
+
+  // Handle logout
+  function logout() {
+    setCurrentUser(null);
+    setToken(null);
+  }
+
   return (
     <div className="App">
       <Router>
-        <NavBar />
-        <Routes>
-          <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/meals" element={<Meals />}></Route>
-          <Route exact path="/bmi" element={<BMICalculator />}></Route>
-          <Route exact path="/login" element={<Login />}></Route>
-          <Route exact path="/signup" element={<Signup />}></Route>
-        </Routes>
+        <UserContext.Provider
+          value={{ currentUser, setCurrentUser }}>
+          <NavBar logout={logout} />
+          <RoutesComponents login={login} signUp={signUp} />
+        </UserContext.Provider>
       </Router>
     </div>
   );
