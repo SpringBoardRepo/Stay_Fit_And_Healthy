0edfcee1cf3562a649f814fd1220daf3fd4ea787
poorvarathore05 <poorvarathore05@gmail.com>
2021-12-29 18:52:52 -0800

    made routes component

---
+++ Frontend/src/RoutesComponents.js
@@ -0,0 +1,21 @@
+import { Redirect, Route, Routes } from "react-router-dom";
+import Meals from './Meals';
+import BMICalculator from './BMICalculator';
+import Login from './Login'
+import Signup from './Signup';
+import Home from './Home';
+
+function RoutesComponents({ login, signUp }) {
+    return (
+        <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/meals" element={<Meals />}></Route>
+            <Route exact path="/bmi" element={<BMICalculator />}></Route>
+            <Route exact path="/login" element={<Login login={login} />}></Route>
+            <Route exact path="/signup" element={<Signup signUp={signUp} />}></Route>
+            {/* <Redirect to="/" /> */}
+        </Routes>
+    )
+}
+
+export default RoutesComponents;
