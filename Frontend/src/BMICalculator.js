d063e15bb751ce2216ed4054e5fb9d25c3cc72ef
poorvarathore05 <poorvarathore05@gmail.com>
2022-01-02 19:28:48 -0800

    cleanedUp component

--- Frontend/src/BMICalculator.js
+++ Frontend/src/BMICalculator.js
@@ -1,8 +1,12 @@
 
 import { useState } from 'react';
-import { Card, Form, FormLabel } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { Button } from 'reactstrap'
+import ActivityLevel from './ActivityLevel';
 import "./BmiForm.css";
+import BmrForm from './BmrForm';
+import DietPlan from './DietPlan';
+
 function BMICalculator() {
 
     const INTIAL_DATA = {
@@ -16,6 +20,8 @@ function BMICalculator() {
 
     const [bmrData, setBmrData] = useState(INTIAL_DATA);
     const [bmr, setBmr] = useState('');
+    const [calories, setCalories] = useState('');
+    const [dietPlan, setDietPlan] = useState('');
 
     const handleChange = (evt) => {
         const { name, value } = evt.target;
@@ -43,11 +49,11 @@ function BMICalculator() {
         let h = ((heightINFeet * 30.48) + (heightInInches * 2.54));
 
         if (gender === 'female') {
-            bmr = 655 + (4.35 * weight) + (4.7 * h) - (4.7 * age);
+            bmr = (655 + (4.35 * weight) + (4.7 * h) - (4.7 * age)).toFixed(2);
             setBmr(bmr);
         }
         else if (gender === 'male') {
-            bmr = 66 + (6.23 * weight) + (12.7 * h) - (6.8 * age);
+            bmr = (66 + (6.23 * weight) + (12.7 * h) - (6.8 * age)).toFixed(2);
             setBmr(bmr);
         }
         setBmrData(INTIAL_DATA);
@@ -55,82 +61,115 @@ function BMICalculator() {
 
     const handleBMRSubmit = (event) => {
         event.preventDefault();
+        let totalCalories;
+        const data = new FormData(event.currentTarget);
+        const dataValue = data.get('activityLevel');
+        if (dataValue === 'little or no exercise') {
+            totalCalories = Math.floor(bmr * 1.2);
+            setCalories(totalCalories);
+        }
+        else if (dataValue === 'light exercise/sports 1-3 days/week') {
+            totalCalories = Math.floor(bmr * 1.375);
+            setCalories(totalCalories);
+        }
+        else if (dataValue === 'moderate exercise/sports 3-5 days/week') {
+            totalCalories = Math.floor(bmr * 1.55);
+            setCalories(totalCalories);
+        }
+        else if (dataValue === 'hard exercise/sports 6-7 days a week') {
+            totalCalories = Math.floor(bmr * 1.725);
+            setCalories(totalCalories);
+        }
+        else if (dataValue === 'very hard exercise/sports and physical job or 2x training') {
+            totalCalories = Math.floor(bmr * 1.9);
+            setCalories(totalCalories);
+        }
+    }
+
+    const handleDietPlanSubmit = (event) => {
+        event.preventDefault();
+        let totalCalories = calories;
+        const data = new FormData(event.currentTarget);
+        const dietPlanValue = data.get('dietPlan');
+
+        if (dietPlanValue === 'Maintain current weight; 0 calories deficit') {
+            setDietPlan(totalCalories);
+        }
+        else if (dietPlanValue === 'Gain .5lb per week; 250 calories surplus') {
+            totalCalories += 250;
+            setDietPlan(totalCalories);
+        }
+        else if (dietPlanValue === 'Gain 1lb per week; 500 calories surplus') {
+            totalCalories += 500;
+            setDietPlan(totalCalories);
+        }
+        else if (dietPlanValue === 'Gain 1.5lb per week; 750 calories surplus') {
+            totalCalories += 750;
+            setDietPlan(totalCalories);
+        }
+        else if (dietPlanValue === 'Lose .5lb per week; 250 calories deficit') {
+            totalCalories -= 250;
+            setDietPlan(totalCalories);
+        }
+        else if (dietPlanValue === 'Lose 1lb per week; 500 calories deficit') {
+            totalCalories -= 500;
+            setDietPlan(totalCalories);
+        }
+        else if (dietPlanValue === 'Lose 1.5lb per week; 750 calories deficit') {
+            totalCalories -= 750;
+            setDietPlan(totalCalories);
+        }
     }
     return (
-
         <Card style={{ width: '22rem' }}>
             <h4 className="Title">BMR &amp; Calories Calculator</h4>
-            <Form className="form" onSubmit={handleSubmit}>
-                <Form.Group className="mb-3">
-                    <FormLabel><b>Weight</b></FormLabel>
-                    <Form.Control type="text"
-                        placeholder="Enter Your Weight in Pounds"
-                        name="weight"
-                        required
-                        id="weight"
-                        autoFocus
-                        value={bmrData.weight}
-                        onChange={handleChange} />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label><b>Height</b></Form.Label>
-                    <Form.Control type="text" placeholder="Enter Your Height Ex.5'0"
-                        required
-                        id="height"
-                        name="height"
-                        value={bmrData.height}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label><b>Age</b></Form.Label>
-                    <Form.Control type="text" placeholder="Enter Your Age"
-                        required
-                        id="age"
-                        name="age"
-                        value={bmrData.age}
-                        onChange={handleChange}
-                    />
-                </Form.Group>
-                <Form.Group className="mb-3" >
-                    <Form.Label><b>Gender</b></Form.Label>
-                    <Form.Select name="gender" value={bmrData.gender} onChange={handleChange}>
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                    </Form.Select>
-                </Form.Group>
-                <Button type="submit" size="md" block>Calculate BMR</Button>
-                {bmr ? <div><Button className="BmrBtn" color="info" size="md" block outline>{bmr}</Button></div> : null}
-            </Form>
+            <BmrForm
+                value={bmrData.weight, bmrData.height, bmrData.age, bmrData.gender}
+                handleSubmit={handleSubmit}
+                handleChange={handleChange}
+            />
             {bmr ?
-                <Form className="form" onSubmit={handleBMRSubmit}>
-                    <Form.Group className="mb-3">
-                        <Form.Label><b>Activity Level</b></Form.Label>
-                        <Form.Select name="activityLevel"
-                            id="activityLevel"
-                            value={bmrData.activityLevel}
-                            onChange={handleChange}>
-                            <option value="little or no exercise">Little or no exercise</option>
-                            <option value="light exercise/sports 1-3 days/week">Light exercise/sports 1-3 days/week</option>
-                            <option value="moderate exercise/sports 3-5 days/week">Moderate exercise/sports 3-5 days/week</option>
-                            <option value="hard exercise/sports 6-7 days a week">Hard exercise/sports 6-7 days a week</option>
-                            <option value="very hard exercise/sports and physical job or 2x training">Very hard exercise/sports and physical job or 2x training</option>
-                        </Form.Select>
-                    </Form.Group>
-                    <Button type="submit" size="md" block>Calculate Calories</Button>
-                </Form> : null
+                <div><Button className="Btn"
+                    color="success"
+                    size="md"
+                    block >
+                    Your BMR : {bmr}
+                </Button>
+                </div>
+                : null}
+            {bmr ?
+                <ActivityLevel
+                    name="activityLevel"
+                    value={bmrData.activityLevel}
+                    handleChange={handleChange}
+                    handleBMRSubmit={handleBMRSubmit} /> : null
             }
-            <Form.Group className="mb-3">
-                <Form.Label><b>Diet Plan</b></Form.Label>
-                <Form.Select name="dietPlan" value={bmrData.dietPlan} onChange={handleChange}>
-                    <option value="Maintain current weight; 0 calories deficit">Maintain current weight; 0 calories deficit</option>
-                    <option value="Gain 1.5lbs per week; 750 calories surplus">Gain 1.5lbs per week; 750 calories surplus</option>
-                    <option value="Lose 1lb per week; 500 calories deficit">Lose 1lb per week; 500 calories deficit</option>
-                    <option value="Lose 1.5lbs per week; 750 calories deficit">Lose 1.5lbs per week; 750 calories deficit</option>
-                    <option value="Gain .5lb per week; 250 calories surplus">Gain .5lb per week; 250 calories surplus</option>
-                    <option value="Gain 1lb per week; 500 calories surplus">Gain 1lb per week; 500 calories surplus</option>
-                </Form.Select>
-            </Form.Group>
+            {calories ?
+                <div>
+                    <Button className="Btn"
+                        color="success"
+                        size="md"
+                        block >
+                        Total Calories : {calories}
+                    </Button>
+                </div>
+                : null}
+            {calories ?
+                <DietPlan name="dietPlan"
+                    value={bmrData.dietPlan}
+                    handleChange={handleChange}
+                    handleDietPlanSubmit={handleDietPlanSubmit} />
+                : null}
+            {dietPlan ?
+                <div>
+                    <Button className="Btn"
+                        color="success"
+                        size="md"
+                        block >
+                        Total Calories You need : {dietPlan}
+                    </Button>
+                </div>
+                : null}
         </Card>
     );
 
