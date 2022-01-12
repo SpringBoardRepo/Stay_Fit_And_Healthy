40401e2c3f4c12893415a9fd2f54747e77026c8c
poorvarathore05 <poorvarathore05@gmail.com>
2022-01-11 18:30:27 -0800

    added loading spinner

---
+++ Frontend/src/LoadingSpinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LoadingSpinner.css";
+
+
+function LoadingSpinner() {
+    return (
+        <div className="loader">
+            Loading ...
+        </div>
+    );
+}
+
+export default LoadingSpinner;
